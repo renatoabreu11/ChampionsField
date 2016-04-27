c8a69e594f43423514ebbd3ff7396ffebf72682e
rsafeup <renatoabreu__@hotmail.com>
2016-04-27 12:57:03 +0100

    initial commit with project files

---
+++ ChampionsField/android/src/com/renaros/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.renaros.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.renaros.game.ChampionsField;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChampionsField(), config);
+	}
+}
